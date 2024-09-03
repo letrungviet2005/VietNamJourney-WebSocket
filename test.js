4724c8595302afa1460ef07818017484edb9e574
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-09-03 19:52:16 +0700

    new

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 
 // Thay thế URL bằng địa chỉ IP và cổng của bạn
-const ws = new WebSocket('ws://103.130.213.138:8866');
+const ws = new WebSocket('ws://localhost:8866');
 
 // Khi kết nối WebSocket mở
 ws.on('open', () => {
