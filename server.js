4a908c171a17c02994fd71ab85281abdfc4a27f8
letrungviet2005 <viet.lt.23it@vku.udn.vn>
2024-09-04 11:33:01 +0700

    new

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const https = require('https');
+const fs = require('fs');
 const WebSocket = require('ws');
-const { handleMessage, handleClientClose } = require('./handler');
-const { handleStream } = require('./streamHandler');
 
-const wss = new WebSocket.Server({ port: 8080 });
+// Đọc chứng chỉ SSL và khóa riêng
+const serverOptions = {
+    cert: fs.readFileSync('bwdjourney_id_vn_c5857_b8ff5_1729088192_a902cc4aa08960412aac9f299eec9eb6.crt'),
+    key: fs.readFileSync('c5857_b8ff5_b1d0fc444fa13dbd56b605a652d71aef.key'),
+};
+
+// Tạo server HTTPS
+const server = https.createServer(serverOptions);
+
+// Tạo WebSocket server
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
@@ -14,7 +24,7 @@ wss.on('connection', (ws) => {
                 handleMessage(ws, message, wss); 
             }
         } catch (error) {
-            handleStream(ws, message,wss);
+            handleStream(ws, message, wss);
         }
     });
 
@@ -24,4 +34,7 @@ wss.on('connection', (ws) => {
     });
 });
 
-console.log('WebSocket server is running on ws://103.130.213.138:8080');
+// Lắng nghe kết nối trên cổng 443 cho HTTPS
+server.listen(8080, () => {
+    console.log('WebSocket server is running on Server');
+});
